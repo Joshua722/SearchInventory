95634fe5b661c6fc73ee386f46d1aa5a979d87da
Joshua <Joshua32149@gmail.com>
2023-10-19 10:31:48 -0400

    Finished second bonus

--- src/main/java/SearchInventory.java
+++ src/main/java/SearchInventory.java
@@ -1,34 +1,58 @@
-import java.sql.Array;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class SearchInventory {
+    public static ArrayList<Product> inventory = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        ArrayList<Product> inventory = getInventory();
+        inventoryReader();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("We carry the following inventory: ");
+        Collections.sort(inventory, (p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName()));
+
 
         for(int i = 0; i < inventory.size(); i++){
             Product p = inventory.get(i);
 
-            System.out.printf("id:%d %s - Price: $%.2f ",p.getId(),p.getName(), p.getPrice());
+            System.out.printf("id:%d %s - Price: $%.2f \n",p.getId(),p.getName(), p.getPrice());
+
 
         }
+        getInventory();
     }
-    public static ArrayList<Product> getInventory(){
-        ArrayList<Product> inventory = new ArrayList<Product>();
-        inventory.add(new Product(22,"Milk",2.33F));
-        inventory.add(new Product(23,"Butter",4.23F));
-        inventory.add(new Product(24,"Cheese",3.23F));
-        inventory.add(new Product(25,"Eggs",1.23F));
-        inventory.add(new Product(26,"Watermelon",.59F));
+    public static void inventoryReader(){
+        String input;
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        Collections.sort(inventory, (p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName()));
+            while((input = bufferedReader.readLine()) != null){
+                String[] inventoryCSV = input.split("\\|");
+                int id = Integer.parseInt(inventoryCSV[0]);
+                String name = inventoryCSV[1];
+                double productPrice = Double.parseDouble(inventoryCSV[2]);
+                inventory.add(new Product(id,name,(float)productPrice));
+            }
+            fileReader.close();
+            bufferedReader.close();
+        }
 
-
-        return inventory;
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
+
+    public static void getInventory(){
+
+        for(Product product: inventory)
+
+            System.out.println(product.getId());
+    }
 }
